6544ed5a6997bc075849b273135307ad4008fc3f
bscherer <scherer.benedikt@gmail.com>
2017-04-23 22:12:59 +0200

    init test

---
+++ crawling/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$tour_operators = [
+	[
+		"name" => "quertour",
+		"url" => ["http://www.quertour.de/"],
+		"type" => "html"
+	],
+	[
+		"name" => "onlineweg",
+		"url" => ["https://www.onlineweg.de/"],
+		"type" => "html"
+	]
+];
